4ec173a6b63227b3a1167a752dea1d9cc4fce88e
jmchenry23 <113141251+jmchenry23@users.noreply.github.com>
2022-09-12 14:14:54 -0600

    Add files via upload

---
+++ main-script.js
@@ -0,0 +1,14 @@
+
+function getImage(){
+    var picSpot = document.getElementById("coolPhoto");
+
+    picSpot.innerHTML="<img src = 'images/sword.jpg'/>";
+
+}
+
+function hideImage(){
+    var picSpot = document.getElementById("coolPhoto");
+
+    picSpot.innerHTML="";
+    
+}
